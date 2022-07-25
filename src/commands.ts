29b86939c2033d0f38cf3a56c52ba19bbdce772c
burningalc <kennychiu.v@gmail.com>
2022-07-25 12:28:55 +0800

    initial commit

---
+++ src/commands.ts
@@ -0,0 +1,19 @@
+import { Command } from "commander";
+const program = new Command();
+
+// commands
+import init from "@commands/init";
+
+program
+  .name("mintbox")
+  .description("CLI to quickly create a new NFT project")
+  .version("0.0.1");
+
+program
+  .command("init")
+  .description("init a NFT project from the template")
+  .action((str, options) => {});
+
+program.parse(process.argv);
+
+export default program;
