0c4baa57eea78afc869df0ecbec1c3b8628d30b5
Peter <choimingwang@gmail.com>
2022-07-25 15:06:52 +0800

    updated copy folder logic & bug fixed

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-#!/usr/bin/env node
-import commander from "./commands";
+import { Command } from "commander";
+import path from "path";
+import fse from "fs-extra";
+const program = new Command();
+
+// commands
+// import init from "@commands/init";
+
+program
+  .name("mintbox")
+  .description("CLI to quickly create a new NFT project")
+  .version("0.0.1");
+
+program
+  .command("init")
+  .description("init a NFT project from the template")
+  .action((str, options) => {
+    fse.copy(path.join(__dirname, "bin"), "../", (err: Error) => {
+      err ? console.log(err) : console.log("Successfully installed!");
+    });
+  });
+
+program.parse(process.argv);
+
+export default program;
